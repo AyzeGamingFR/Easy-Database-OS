8e5edbdd992959531d0e89f5dc7efe4135c4371e
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-04-10 09:59:41 +0200

    Create OS-Images.js

---
+++ OS-Images/OS-Images.js
@@ -0,0 +1,3 @@
+var OSBackgroundImage1 = require("./OS-Images/OS-Background-1.jpg");
+var OSBackgroundImage2 = require("./OS-Images/OS-Background-2.jpg");
+var OSBackgroundImage3 = require("./OS-Images/OS-Background-3.jpg");
